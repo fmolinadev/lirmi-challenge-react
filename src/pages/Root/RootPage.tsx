43d6920bfbf9e901dce1a074d17ca63492fcb09d
fmolinadev <franciscomolina.dev@gmail.com>
2025-02-07 21:24:55 -0300

    feat: se agrega boton de ui y navegacion desde enum de rutas

--- src/pages/Root/RootPage.tsx
+++ src/pages/Root/RootPage.tsx
@@ -1,7 +1,20 @@
-import { teachersPicture } from "@/assets/images";
+import { useNavigate } from "react-router-dom";
 import styles from "./rootPage.module.css";
+import { ButtonUI } from "@/components";
+import { teachersPicture } from "@/assets/images";
+import { AppRoutesEnum } from "@/enums";
 
 export const RootPage = () => {
+  const navigate = useNavigate();
+
+  const handleButtonEnter = () => {
+    navigate(AppRoutesEnum.HOME);
+  };
+
+  const handleButtonDemo = () => {
+    window.open(import.meta.env.VITE_DEMO_VIEW, "_blank");
+  };
+
   return (
     <div className={styles["root-layout"]}>
       <div className={styles["image-container"]}>
@@ -19,6 +32,22 @@ export const RootPage = () => {
             Comenzá a optimizar la gestión de tu colegio e impulsar el
             desarrollo de los docentes con Lirmi
           </p>
+        </div>
+        <div className={styles["content-buttons"]}>
+          <ButtonUI
+            variant="primary"
+            onClick={handleButtonEnter}
+            isLoading={false}
+          >
+            Ingresar ahora
+          </ButtonUI>
+          <ButtonUI
+            variant="outlined"
+            onClick={handleButtonDemo}
+            isLoading={false}
+          >
+            Ver demo
+          </ButtonUI>
         </div>
       </div>
     </div>
