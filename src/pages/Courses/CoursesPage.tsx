65fffff844276b878f8e56cb8b5d255707309d84
fmolinadev <franciscomolina.dev@gmail.com>
2025-02-09 17:24:02 -0300

    feat: agregue tablas basicas  con datos en vistas

--- src/pages/Courses/CoursesPage.tsx
+++ src/pages/Courses/CoursesPage.tsx
@@ -1,22 +1,42 @@
+import { GridColDef } from "@mui/x-data-grid";
 import { useCourseStore } from "@/store";
+import { CourseInterface } from "@/interface";
+import { TableUI } from "@/components";
+import { BookIcon } from "@/assets";
 
 export const CoursesPage = () => {
   const { courses } = useCourseStore();
 
+  const columns: GridColDef[] | CourseInterface[] = [
+    { field: "name", headerName: "Nombre", width: 160 },
+    {
+      field: "color",
+      headerName: "Icono",
+      width: 110,
+      renderCell: (params) => <BookIcon color={params.value} />,
+    },
+    {
+      field: "subjects",
+      headerName: "Asig. Vinculadas",
+      width: 140,
+      valueGetter: (params) => {
+        return Array.isArray(params.value) ? params.value.length : 0;
+      },
+    },
+    {
+      field: "students",
+      headerName: "Estud. Vinculados",
+      width: 140,
+      valueGetter: (params) => {
+        return Array.isArray(params.value) ? params.value.length : 0;
+      },
+    },
+  ];
+
   return (
     <div>
       <h1>Cursos</h1>
-      {courses.length > 0 ? (
-        <ul>
-          {courses.map((course) => (
-            <li key={course.id}>
-              {course.name} - {course.subjects}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No hay cursos disponibles.</p>
-      )}
+      <TableUI rows={courses} columns={columns} pageSizeOptions={[5, 10, 15]} />
     </div>
   );
 };
